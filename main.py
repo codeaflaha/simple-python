d8efc453045e0c4f8c8feaff76b9b597e14d97e9
Najamuddin Dwi <dwiajakokrepot@gmail.com>
2020-06-30 18:45:22 +0700

    update .gitignore
    Signed-off-by: najamuddindwi <dwiajakokrepot@gmail.com>

---
+++ main.py
@@ -0,0 +1,9 @@
+import requests
+print("hello word")
+try:
+    r = requests.get('https://google.com')
+    print(r.status_code)
+    if r.status_code == 200:
+        print(r.text)
+except Exception as e :
+    print("ada error", e)
